ae5e4f0e13d6bac2ea7bbf44d4f8df5dfab39e95
Shilpi <shilpisen072002@gmail.com>
2023-01-29 23:20:02 +0530

    initial

---
+++ IDENTIFIERS/validinvalididendifiers.py
@@ -0,0 +1,27 @@
+age=24
+name='shilpi'
+print('age',age)
+print('name',name)
+
+
+last_name='sen'
+_id='10800320070'
+name2='panna'
+
+'''
+1.use only alphanumeric characters and underscores
+2.never start with a number
+3.use underscores to separate two words
+4.keywords cannot be identifires
+
+'''
+
+'''
+invalid identifires
+
+'''
+
+2name='sen'
+marks$=98
+class ='hellow'
+
